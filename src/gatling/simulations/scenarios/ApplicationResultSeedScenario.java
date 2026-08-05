65c0346fd74dcf5cb817199dc422f4df48408e43
Jason Francis <jason.francis@hmcts.net>
2026-08-05 16:51:35 +0100

    Add application result update performance proof

---
+++ src/gatling/simulations/scenarios/ApplicationResultSeedScenario.java
@@ -0,0 +1,31 @@
+package scenarios;
+
+import io.gatling.javaapi.core.ChainBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.StringBody;
+import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.group;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+/** Creates an existing Result on the Application Entry in the session for proof-only setup. */
+public final class ApplicationResultSeedScenario {
+  private static final String INITIAL_RESULT_CODE = "COST";
+  private static final String INITIAL_WORDING_VALUE = "500 Pounds";
+
+  private ApplicationResultSeedScenario() {}
+
+  public static ChainBuilder createExistingResult() {
+    return group("AppReg_055_Application_Result_Seed").on(
+      exec(http("Create proof setup result")
+        .post("/application-lists/#{applicationListId}/entries/#{applicationEntryId}/results")
+        .header("Accept", "application/vnd.hmcts.appreg.v1+json")
+        .header("Content-Type", "application/vnd.hmcts.appreg.v1+json")
+        .header("X-XSRF-TOKEN", "#{xsrfToken}")
+        .body(StringBody("""
+          {"resultCode":"%s","wordingFields":[{"key":"Amount of costs","value":"%s"}]}
+          """.formatted(INITIAL_RESULT_CODE, INITIAL_WORDING_VALUE)))
+        .check(status().in(200, 201)))
+    );
+  }
+}
